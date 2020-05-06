1d2696877f07b945541c3882b85b84af09d0ef53
mickdu62 <mickdu62200@gmail.com>
2020-05-06 10:31:14 +0200

    creation of the controller test 'ListTaskController, set up inside a connection system

--- tests/Controller/ListTaskControllerTest.php
+++ tests/Controller/ListTaskControllerTest.php
@@ -8,16 +8,53 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
 
 class ListTaskControllerTest extends WebTestCase
 {
+    protected $client = null;
+
+    protected function doSetUp()
+    {
+        if ($this->client == null) {
+
+            $this->client = static::createClient();
+        }
+
+        return $this->client;
+    }
+
     public function testListTaskNoLoggedIn()
     {
-        $client = static::createClient();
+        $this->doSetUp();
+        $this->logIn();
 
-        $client->request('GET', '/tasks');
+        $crawler = $this->doSetUp()->request('GET', '/tasks');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(1,$crawler->filter('.tasks')->count());
+    }
 
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    public function logIn()
+    {
+        $session = self::$container->get('session');
+
+        $user = self::$container->get('doctrine')->getRepository(User::class)->findOneBy(['username' => 'username_test']);
+
+        $firewallName = 'main';
+        // if you don't define multiple connected firewalls, the context defaults to the firewall name
+        // See https://symfony.com/doc/current/reference/configuration/security.html#firewall-context
+        $firewallContext = 'main';
+
+        // you may need to use a different token class depending on your application.
+        // for example, when using Guard authentication you must instantiate PostAuthenticationGuardToken
+        $token = new PostAuthenticationGuardToken($user, $firewallName, $user->getRoles());
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->doSetUp()->getCookieJar()->set($cookie);
     }
 }
